6f32600fd46942cf476bcabb4b60c7600e8ea3fb
Yangfeng <smartwalle@gmail.com>
2016-09-09 06:28:02 +0800

    添加 GetValueWithMap

--- filter/filter.go
+++ filter/filter.go
@@ -1,8 +1,8 @@
 package filter
 
 import (
+	"reflect"
 	"github.com/flosch/pongo2"
-	"github.com/smartwalle/container"
 )
 
 func init() {
@@ -10,7 +10,28 @@ func init() {
 }
 
 func GetValueWithMap(in, param *pongo2.Value) (out *pongo2.Value, err *pongo2.Error) {
-	var v = container.GetValueWithMap(in.Interface(), param.Interface())
+	var v = getValueWithMap(in.Interface(), param.Interface())
 	out = pongo2.AsValue(v)
 	return out, err
+}
+
+func getValueWithMap(source, key interface{}) interface{} {
+	if source == nil {
+		return nil
+	}
+
+	var sourceValue = reflect.ValueOf(source)
+	if sourceValue.IsNil() {
+		return nil
+	}
+
+	switch sourceValue.Kind() {
+	case reflect.Map:
+		var targetValue = reflect.ValueOf(key)
+
+		if targetValue.IsValid() {
+			return sourceValue.MapIndex(targetValue).Interface()
+		}
+	}
+	return nil
 }
