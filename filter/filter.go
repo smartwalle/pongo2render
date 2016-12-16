14c19e351017435f02da310d115fbe3c4856145f
SmartWalle <smartwalle@gmail.com>
2016-12-16 08:06:54 +0800

    update

--- filter/filter.go
+++ filter/filter.go
@@ -7,54 +7,49 @@ import (
 )
 
 func init() {
-	pongo2.RegisterFilter("ValueWithMap", ValueWithMap)
-	pongo2.RegisterFilter("HasPrefix", HasPrefix)
-	pongo2.RegisterFilter("HasSuffix", HasSuffix)
-	pongo2.RegisterFilter("EqualToString", EqualToString)
-	pongo2.RegisterFilter("CompareString", CompareString)
+	pongo2.RegisterFilter("ValueWithMap", valueWithMap)
+	pongo2.RegisterFilter("HasPrefix", hasPrefix)
+	pongo2.RegisterFilter("HasSuffix", hasSuffix)
+	pongo2.RegisterFilter("CompareString", compareString)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-func ValueWithMap(in, param *pongo2.Value) (out *pongo2.Value, err *pongo2.Error) {
-	var v = valueWithMap(in.Interface(), param.Interface())
-	out = pongo2.AsValue(v)
-	return out, err
-}
+func valueWithMap(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	var source = in.Interface()
+	var key = param.Interface()
 
-func valueWithMap(source, key interface{}) interface{} {
 	if source == nil {
-		return nil
+		return nil, nil
+	}
+
+	if key == nil {
+		return nil, nil
 	}
 
 	var sourceValue = reflect.ValueOf(source)
 	if sourceValue.IsNil() {
-		return nil
+		return nil, nil
 	}
 
 	switch sourceValue.Kind() {
 	case reflect.Map:
 		var targetValue = reflect.ValueOf(key)
-
 		if targetValue.IsValid() {
-			return sourceValue.MapIndex(targetValue).Interface()
+			return pongo2.AsValue(sourceValue.MapIndex(targetValue).Interface()), nil
 		}
 	}
-	return nil
+	return nil, nil
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-func HasPrefix(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+func hasPrefix(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	return pongo2.AsValue(strings.HasPrefix(in.String(), param.String())), nil
 }
 
-func HasSuffix(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+func hasSuffix(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	return pongo2.AsValue(strings.HasSuffix(in.String(), param.String())), nil
 }
 
-func EqualToString(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
-	return pongo2.AsValue(in.String() == param.String()), nil
-}
-
-func CompareString(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+func compareString(in, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	return pongo2.AsValue(strings.Compare(in.String(), param.String())), nil
 }
